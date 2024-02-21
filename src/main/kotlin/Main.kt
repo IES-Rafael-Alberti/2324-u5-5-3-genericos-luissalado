443397ca943d6f5d7482cf8abf3878d20af80d57
luissalado <114474655+luissalado@users.noreply.github.com>
2024-02-21 21:26:46 +0100

    pila finalizada

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,58 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+class Pila<T> {
+    private val elementos = mutableListOf<T>()
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+    //Tope: devuelve el elemento en la cabeza de la pila.
+    //Push: Insertar o apilar
+    //Pop: Eliminar o desapilar
+    //Vacía: si está vacía (tope es nulo, o tamaño es 0) o llena (si existe tamaño máximo)
+    fun push(elemento: T) {
+        elementos.add(elemento)
+    }
+
+    fun pop(): T? {
+        return if (!isEmpty()) elementos.removeAt(elementos.size - 1) else null
+    }
+
+    fun vacia(): Boolean {
+        return elementos.isEmpty()
+    }
+
+    private fun isEmpty(): Boolean {
+        return elementos.isEmpty()
+    }
+}
+
+fun <T> reverse(lista: List<T>): List<T> {
+    val pila = Pila<T>()
+    val recorre = lista.iterator() //recorrer la lista
+
+    while (recorre.hasNext()) {
+        pila.push(recorre.next())
+    }  //apila elementos en la pila
+
+    val linvertida = mutableListOf<T>()
+
+    while (!pila.vacia()) {
+        linvertida.add(pila.pop()!!)  //Se añade el elemento desapilado
+    }
+
+    return linvertida
+}
+
+fun main() {
+
+    //codigo entregado
+
+
+    val numbers = listOf("one", "two", "three", "four")
+    val numbersRev: List<String> = reverse(numbers)
+
+    if (listOf("four", "three", "two", "one") != numbersRev) {
+        println("Error")
+    } else {
+        println("Correcto")
+    }
+
+    println(numbersRev)
 }
